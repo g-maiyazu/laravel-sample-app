162fe3080b2d71d4316013c0c966932867b64ff8
g-maiyazu <emujei6123@yahoo.co.jp>
2020-06-07 12:25:16 +0900

    Add Create and Show function
    新規登録機能、詳細機能を追加

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\ContactForm;
+
+// クエリビルダ(phpでクエリが書ける)
+use Illuminate\Support\Facades\DB;
+
 class ContactFormController extends Controller
 {
     /**
@@ -13,9 +18,18 @@ class ContactFormController extends Controller
      */
     public function index()
     {
-        // .前でフォルダ指定、.後にファイル名を指定
-        // ⇒contactフォルダのindexファイル
-        return view('contact.index');
+      // エロクワント ORマッパー
+      // $contact = ContactForm::all();
+
+      // クエリビルダ
+      $contacts = DB::table('contact_forms')
+      ->select('id', 'your_name', 'title', 'created_at')
+      ->orderBy('created_at', 'desc')
+      ->get();
+
+      // .前でフォルダ指定、.後にファイル名を指定
+      // ⇒contactフォルダのindexファイル
+      return view('contact.index', compact('contacts'));
     }
 
     /**
@@ -25,7 +39,7 @@ class ContactFormController extends Controller
      */
     public function create()
     {
-        //
+      return view('contact.create');
     }
 
     /**
@@ -36,7 +50,20 @@ class ContactFormController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $contact = new ContactForm;
+
+      $contact->your_name = $request->input('your_name');
+      $contact->title = $request->input('title');
+      $contact->email = $request->input('email');
+      $contact->url = $request->input('url');
+      $contact->gender = $request->input('gender');
+      $contact->age = $request->input('age');
+      $contact->contact = $request->input('contact');
+
+      $contact->save();
+
+      return redirect('contact/index');
+
     }
 
     /**
@@ -47,8 +74,37 @@ class ContactFormController extends Controller
      */
     public function show($id)
     {
-        //
+        $contact = ContactForm::find($id);
+
+        if($contact->gender === 0){
+          $gender = '男性';
+        }
+        if($contact->gender === 1){
+          $gender = '女性';
+        }
+        if($contact->age === 1){
+          $age = '～19歳';
+        }
+        if($contact->age === 2){
+          $age = '20歳～29歳';
+        }
+        if($contact->age === 3){
+          $age = '30歳～39歳';
+        }
+        if($contact->age === 4){
+          $age = '40歳～49歳';
+        }
+        if($contact->age === 5){
+          $age = '50歳～59歳';
+        }
+        if($contact->age === 6){
+          $age = '60歳～';
+        }
+
+        return view('contact.show', compact('contact','gender','age'));        
     }
+
+
 
     /**
      * Show the form for editing the specified resource.
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('tests/test', 'TestController@index');
 // また、ユーザー認証できたら、各ページに飛ぶ流れになる
 Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
   Route::get('index', 'ContactFormController@index')->name('contact.index');
+  Route::get('create', 'ContactFormController@create')->name('contact.create');
+  Route::post('store', 'ContactFormController@store')->name('contact.store');
+  Route::get('show/{id}', 'ContactFormController@show')->name('contact.show');
 });
 
 // Route::resource('contacts', 'ContactFormController');
